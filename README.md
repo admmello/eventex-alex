626295994d63684ba1e68e5b69db42b9f703710a
admmello <admmello@gmail.com>
2018-12-26 21:41:00 -0500

    Refatora geral

---
+++ README.md
@@ -0,0 +1,40 @@
+# Eventex
+
+Sistema de eventos encomendado pela Morena
+
+## Como desenvolver?
+
+1. Clone o repositório.
+2. Crie um virtualenv com Python 3.5
+3. Ative o virtualenv.
+4. Instale as dependências.
+5. Configure a instância com o .env
+6. Execute os testes.
+
+```console
+git clone https://github.com/admmello/eventex-alex.git
+cd wttd
+python -m venv .wttd
+source .wttd/bin/activate
+pip install -r requirements.txt
+cp contrib/env-sample .env
+python manage.py test
+```
+
+## Como fazer deploy?
+
+1. Crie uma instancia no heroku.
+2. Envie as configurações para o heroku.
+3. Define um SECRET_KEY segura para instancia.
+4. Defina DEBUG=False
+5. Configure o serviço de email.
+6. Envie o código para o heroku.
+
+```console
+heroku create minhainstancia
+heroku config:push
+heroku config:set SECRET_KEY=`python contrib/secret_gen.py`
+heroku config:set DEBUG=False
+# configuro e-mail
+git push heroku master --force 
+```
---
+++ eventex/core/tests/test_mail_subscribe.py
@@ -0,0 +1,36 @@
+from django.core import mail
+from django.test import TestCase
+
+
+class SubscribePostValid(TestCase):
+    def setUp(self):
+        data = dict(name='nome', cpf='12345678901', email='a@a.com', phone='121212')
+        self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_subscription_email_subject(self):
+        email = mail.outbox[0]
+        expect = 'Confirmação de  inscrição'
+
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_from(self):
+        expect = 'contato@eventex.com.br'
+
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+        expect = ['contato@eventex.com.br', 'a@a.com']
+
+        self.assertEqual(expect, self.email.to)
+
+    def test_subscription_email_body(self):
+        contents = [
+            'nome',
+            '12345678901',
+            'a@a.com',
+            '121212',
+        ]
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
