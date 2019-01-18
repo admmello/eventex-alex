40808e443cbc6a7332f9d2c7b0d99a329db54ae3
admmello <admmello@gmail.com>
2019-01-18 18:15:02 -0500

    email ou telefone opcionais

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -78,3 +78,8 @@ class SubscriptionsNewPostInvalid(TestCase):
         self.assertFalse(Subscription.objects.exists())
 
 
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(name='Nome Teste', cpf='12345678901')
+        response = self.client.post(resolve_url('subscriptions:new'), invalid_data)
+        self.assertContains(response, '<ul class="errorlist nonfield">')
